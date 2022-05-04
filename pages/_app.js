70c7fb60559ca48f15506f82d4f743d3ff7844a9
maxcore25 <mkorobov2002@mail.ru>
2022-05-04 15:05:42 +0300

    Moralis Provider Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { MoralisProvider } from 'react-moralis';
+import { RobinhoodProvider } from '../context/RobinhoodContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.MORALIS_SERVER_URL}
+      appId={process.env.MORALIS_APP_ID}>
+      <RobinhoodProvider>
+        <Component {...pageProps} />
+      </RobinhoodProvider>
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
