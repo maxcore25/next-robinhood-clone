93134ad50e828f9d6597e8feca5ac29e04452323
maxcore25 <mkorobov2002@mail.ru>
2022-05-06 10:27:57 +0300

    Other Smart Contracts Deployment Written

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -4,4 +4,16 @@ const main = async () => {
   const dogeFactory = await ethers.getContracyFactory('Dogecoin');
   const dogeContract = await dogeFactory.deploy();
   console.log('Dogecoin deployed to:', dogeContract.address);
+
+  const bitcoinFactory = await ethers.getContracyFactory('Bitcoin');
+  const bitcoinContract = await bitcoinFactory.deploy();
+  console.log('Bitcoin deployed to:', bitcoinContract.address);
+
+  const solanaFactory = await ethers.getContracyFactory('Solana');
+  const solanaContract = await solanaFactory.deploy();
+  console.log('Solana deployed to:', solanaContract.address);
+
+  const usdcFactory = await ethers.getContracyFactory('Usdc');
+  const usdcContract = await usdcFactory.deploy();
+  console.log('USDC deployed to:', usdcContract.address);
 };
